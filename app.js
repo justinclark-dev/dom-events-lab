0fab1a9b1a7bf31f04a841d7e4ec4ed3fef2c3ef
justinclark-dev <justinclark.dev@gmail.com>
2025-10-01 12:48:36 -0700

    adds ability to select numbers

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+'use strict'
+
 /*-------------------------------- Constants --------------------------------*/
+const buttons = document.querySelectorAll('.button')
 
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        // This log is for testing purposes to verify we're getting the correct value
+        console.log(event.target.innerText)
+        // Future logic to capture the button's value would go here...
+    })
+})
 /*-------------------------------- Functions --------------------------------*/
